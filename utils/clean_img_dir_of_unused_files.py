4803ad3329c108d4641236d8395056ebf8c720d5
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2020-12-21 16:55:33 +0100

    before delete

--- utils/clean_img_dir_of_unused_files.py
+++ utils/clean_img_dir_of_unused_files.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-print("ok");
-
 import os
 import re
 
 dic = {}
-imgs = os.listdir('img')
-for i in imgs:
-    dic[i] = False
+
+SVGs = os.listdir('img/svg')
+for s in SVGs:
+    # print(s)
+    dic[s] = "svg"
+
+PNGs = os.listdir('img/png')
+for p in PNGs:
+    # print(p)
+    dic[p] = "png"
 
 def findfiles(path, regex):
     regObj = re.compile(regex)
@@ -21,55 +26,13 @@ def findfiles(path, regex):
 files = findfiles('.', r'.*(js|css|html|md)')
 
 for f in files:
-    for i in imgs: 
+    for i in list(dic): # https://www.geeksforgeeks.org/python-delete-items-from-dictionary-while-iterating/
         for line in open(f, 'r'):
             if re.search(i, line):
-                print(line)
+                # print(line)
                 dic.pop(i, None)
 
 for k,v in dic.items():
-    print(k)
-    # os.remove('img/'+k)
-
-
-# # #!/usr/bin/env python3
-# # import os
-# # import re
-# # import subprocess
-
-# # def findfiles(path, regex):
-# #     regObj = re.compile(regex)
-# #     res = []
-# #     for root, dirs, fnames in os.walk(path):
-# #         for fname in fnames:
-# #             if regObj.match(fname):
-# #                 res.append(os.path.join(root, fname))
-# #     return res
-
-# # arr = findfiles('.', r'.*\.(js|html|css)')
-# # imgs = os.listdir('img')
-
-# # d = {}
-# # for i in imgs:
-# #     d[i] = False
-
-# # for f in arr:
-# #     good = False
-# #     for i in imgs:
-# #         for line in open(f).readlines():
-# #             if re.search(i, line):
-# #                 good = True
-# #                 print(line)
-# #         if good:
-# #             print('REMOVED ' + i + ' CONTAINED IN ' + f)
-# #             d.pop(i, None)
-# #             good = False
-# #     if good:
-# #         print(f)
-# #         print('---------')
-# #         print()
-
-# # for k,v in sorted(d.items()):
-# #     print(k)
-# #     # UNCOMMENT ME
-# #     # os.remove('img/'+k)
+    print('img', v, k)
+    # UNCOMMENT ME
+    os.remove('img/' + v + '/' + k )
